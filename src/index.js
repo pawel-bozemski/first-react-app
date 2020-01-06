7bc1e088531533adebe627950502f2ee72b5220a
Pawel Bozemski <paw.boz@gmail.com>
2020-01-06 17:21:27 +0100

    Fix procfile

--- src/index.js
+++ src/index.js
@@ -6,5 +6,4 @@ import App from './components/App/AppContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
